9a89ce545b917d1c8e66ffb331f2973404800b18
Michael Tanczos <tanczosm@eastonsd.org>
2020-10-01 09:59:54 -0400

    Added code to include a bindable property

--- CookieClicker/CookieClicker/CookieClicker/MainPage.xaml.cs
+++ CookieClicker/CookieClicker/CookieClicker/MainPage.xaml.cs
@@ -2,19 +2,46 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace CookieClicker
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
     {
         private int count = 0;
+
+
+        public int Count
+        {
+            set
+            {
+                count++;
+                OnPropertyChanged("Count");
+                OnPropertyChanged("Message");
+            }
+            get
+            {
+                return count;
+            }
+        }
+
+        public string Message
+        {
+            get
+            {
+                return $"You have {count} cookies!";
+            }
+        }
 
         public MainPage()
         {
             InitializeComponent();
+
+            this.BindingContext = this;
         }
 
         private async void Button_Clicked(object sender, EventArgs e)
@@ -29,11 +56,36 @@ namespace CookieClicker
 
         private async void PressCookie()
         {
-            count++;
-            MyLabel.Text = "Cookie Pressed " + count + " Times!";
+            Count++;
+            //MyLabel.Text = "Cookie Pressed " + count + " Times!";
 
             await Cookie.ScaleTo(1.2, 125U, Easing.CubicInOut);
             await Cookie.ScaleTo(1.0, 125U, Easing.CubicInOut);
         }
+
+        protected bool SetProperty<T>(ref T backingStore, T value,
+            [CallerMemberName] string propertyName = "",
+            Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
     }
 }
